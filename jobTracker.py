1cb76afb14a00ae9b3d03f010abad594ffbe35c3
BrandonEmbleton <brandonembleton04@gmail.com>
2026-03-06 22:27:12 -0500

    add job creation

--- jobTracker.py
+++ jobTracker.py
@@ -1,5 +1,15 @@
+jobList = []
+
 class job():
-    def __init__(self, title, company, status):
+    def __init__(self, title, company):
         self.title = title
         self.company = company
         self.status = "Applied"
+
+def createJob():
+    title = input("Job Title: ")
+    company = input("Company Name: ")
+    newJob = job(title, company)
+
+
+
