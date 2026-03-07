3584b79908a2fed99be31654280b84c38296341a
BrandonEmbleton <brandonembleton04@gmail.com>
2026-03-06 23:04:10 -0500

    add printJob

--- jobTracker.py
+++ jobTracker.py
@@ -1,15 +1,22 @@
-jobList = []
+jobList = {}
 
 class job():
+
     def __init__(self, title, company):
         self.title = title
         self.company = company
         self.status = "Applied"
+    
+    def printJob(self):
+        print(f"Company: {self.company}")
+        print(f"Title: {self.title}")
+        print(f"Status: {self.status}")
 
 def createJob():
     title = input("Job Title: ")
     company = input("Company Name: ")
     newJob = job(title, company)
+
 
 
 
