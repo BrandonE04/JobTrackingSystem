0d6cc33275f60bab427e5812ca808fc2633afcc5
BrandonEmbleton <brandonembleton04@gmail.com>
2026-03-07 16:27:16 -0500

    implement Id class

--- jobTracker.py
+++ jobTracker.py
@@ -1,13 +1,24 @@
 jobList = {}
 
+class Id():
+    def __init__(self):
+        self.value = 1
+    
+    def incrementId(self):
+        self.value += 1
+
+curId = Id()
+
 class job():
 
-    def __init__(self, title, company):
+    def __init__(self, title, company, id):
         self.title = title
         self.company = company
         self.status = "Applied"
+        self.id = id
     
     def printJob(self):
+        print(f"Id: {self.id}")
         print(f"Company: {self.company}")
         print(f"Title: {self.title}")
         print(f"Status: {self.status}")
@@ -15,8 +26,6 @@ class job():
 def createJob():
     title = input("Job Title: ")
     company = input("Company Name: ")
-    newJob = job(title, company)
-
-
-
+    newJob = job(title, company, curId.value)
+    curId.incrementId()
 
